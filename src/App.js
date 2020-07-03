2c9731f86127066c1408604b43f97160d3126ca8
Julian <jamignott@gmail.com>
2020-07-02 23:26:29 -0400

    add chart

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './routes/Home';
 import Footer from './components/Footer';
+import HistoryChart from './components/HistoryChart';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <Home />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+const App = () => {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      <Home />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/historychart" exact component={HistoryChart} />
+        <Route render={() => <h1>404 Not found</h1>} />
+      </Switch>
       <Footer />
-    </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,12 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand navbar-dark">
-      <a className="navbar-brand" href="localhost:3000">
+      <a className="navbar-brand" href="/">
         ExchangeRates
       </a>
       <div className="navbar-nav">
-        <a className="nav-item nav-link active" href="localhost:3000">
-          Converter <span className="sr-only">(current)</span>
-        </a>
-        <a className="nav-item nav-link active" href="localhost:3000">
-          Base Rates
+        <a className="nav-item nav-link pb-1 active" href="/historychart">
+          Historical Data <span className="sr-only">(current)</span>
         </a>
       </div>
     </nav>
